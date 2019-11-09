d4b2420f4c982a1e0cdc767c9e6edda7b3d6c4e6
Akash <akash.munagala@gmail.com>
2019-11-08 18:10:19 -0800

    Disable depth testing when rendering HUD
    Used to clear depth buffer whenever rendering HUD

--- Voxel-Engine/HUD.cpp
+++ Voxel-Engine/HUD.cpp
@@ -62,6 +62,9 @@ HUD::HUD() : VAO(createVAO()), shader("hud"),
 void HUD::render() {
 	glBindVertexArray(VAO);
 
-	glClear(GL_DEPTH_BUFFER_BIT);
+	glDepthMask(GL_FALSE);
+	glDisable(GL_DEPTH_TEST);
 	crosshair.render();
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);
 }
